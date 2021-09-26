2bb3bcf769f7140de7c14124abcf358e337c2062
Fernando <fmti91@hotmail.com>
2021-09-26 13:28:21 -0300

    Removed signInAnonymously function. Created createUserWithEmailAndPassword and signInWithEmailAndPassword functions.

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -8,12 +8,33 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
+  String _email = "";
+  String _password = "";
+
 class _LoginPageState extends State<LoginPage> {
 
   Future<void> _createUser() async {
     try {
       //Anonymously
-      UserCredential userCredential = await FirebaseAuth.instance.signInAnonymously();
+      //UserCredential userCredential = await FirebaseAuth.instance.signInAnonymously();
+      //print("E-mail: $_email");
+      //print("Password: $_password");
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
+      print("User: $userCredential");
+    } on FirebaseAuthException catch(e) {
+      print("Error: $e");
+    } catch(e) {
+      print("Error: $e");
+    }
+  }
+
+  Future<void> _loginUser() async {
+    try {
+      //Anonymously
+      //UserCredential userCredential = await FirebaseAuth.instance.signInAnonymously();
+      //print("E-mail: $_email");
+      //print("Password: $_password");
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
       print("User: $userCredential");
     } on FirebaseAuthException catch(e) {
       print("Error: $e");
@@ -26,12 +47,43 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("LoginPage"),
+        title: const Text("LoginPage"),
       ),
-      body: Center(
-        child: MaterialButton(
-          onPressed: _createUser,
-          child: Text("Create New Account"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              onChanged: (value) {
+                _email = value;
+              },
+              decoration: const InputDecoration(
+                hintText: "E-mail Field"
+              ),
+            ),
+            TextField(
+              onChanged: (value) {
+                _password = value;
+              },
+              decoration: const InputDecoration(
+                  hintText: "Password Field"
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  onPressed: _loginUser,
+                  child: const Text("Login"),
+                ),
+                MaterialButton(
+                  onPressed: _createUser,
+                  child: const Text("Create New Account"),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
